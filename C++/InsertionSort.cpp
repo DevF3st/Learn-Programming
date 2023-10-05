34910774e54cd4bb7738d2d81f6f5284d780aa8c
Anjali Jahagirdar <115176470+Anjalijahagirdar@users.noreply.github.com>
2023-10-05 18:50:54 +0530

    Update InsertionSort.cpp
    I have make some changes in code

--- C++/InsertionSort.cpp
+++ C++/InsertionSort.cpp
@@ -1,50 +1,35 @@
-
-// C++ program for insertion sort
- 
-#include <bits/stdc++.h>
+#include<iostream>
 using namespace std;
- 
-// Function to sort an array using
-// insertion sort
-void insertionSort(int arr[], int n)
-{
-    int i, key, j;
-    for (i = 1; i < n; i++)
-    {
-        key = arr[i];
-        j = i - 1;
- 
-        // Move elements of arr[0..i-1], 
-        // that are greater than key, to one
-        // position ahead of their
-        // current position
-        while (j >= 0 && arr[j] > key)
-        {
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
 }
- 
-// A utility function to print an array
-// of size n
-void printArray(int arr[], int n)
-{
-    int i;
-    for (i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+void insertionSort(int *array, int size) {
+   int key, j;
+   for(int i = 1; i<size; i++) {
+      key = array[i];//take value
+      j = i;
+      while(j > 0 && array[j-1]>key) {
+         array[j] = array[j-1];
+         j--;
+      }
+      array[j] = key;   //insert in right place
+   }
 }
- 
-// Driver code
-int main()
-{
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int N = sizeof(arr) / sizeof(arr[0]);
- 
-    insertionSort(arr, N);
-    printArray(arr, N);
- 
-    return 0;
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];    //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   insertionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
 }
+
