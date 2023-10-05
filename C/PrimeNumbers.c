be01d07784481df7f2b064ebe21b6de69a0e58b9
ErangaSenavirathne <erangasenevirathna@gmail.com>
2023-10-05 13:02:28 +0530

    C Prime Numbers

---
+++ C/PrimeNumbers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int checkPrime(int num)
+{
+    // 0, 1 and negative numbers are not prime
+    if(num < 2){
+        return 0;
+    }
+    else{   
+    // no need to run loop till num-1 as for any number x the numbers in
+    // the range(num/2 + 1, num) won't be divisible anyways. 
+    // Example 36 wont be divisible by anything b/w 19-35
+        int x = num/2;
+        for(int i = 2; i <=x; i++)
+        {
+            if(num % i == 0)
+            {
+                return 0;
+            }
+        }
+    }
+    // the number would be prime if we reach here
+    return 1;
+}
+
+int main()
+{
+    int a = 1, b = 100;
+    
+    for(int i=a; i <= b; i++){
+        if(checkPrime(i))
+            printf("%d ",i);
+    }
+ 
+    return 0;
+}
